fd9c52e4f7cc035b3a2fe23a72a1069528c00f5e
unknown <avatar0527@gmail.com>
2021-09-11 22:29:32 +0900

    update menu-items to naviage withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+            </Switch>
         </div>
     );
 }
